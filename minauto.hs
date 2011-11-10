437dc4b254bc7a2585993d96f817dec13904deb4
Paul Walger <metaxy@walger.name>
2011-11-10 14:01:09 +0100

    smaller

--- minauto.hs
+++ minauto.hs
@@ -6,47 +6,40 @@ type Table = [((State,State),Bool)]
 
 q :: [State]
 q = [0,1,2,3,4]
-sigma =  [0,1]
+sigma = [0,1]
 f = [4]
-δ :: State -> Input -> State
-δ 0 0 = 1
-δ 0 1 = 2
-δ 1 0 = 4
-δ 1 1 = 2
-δ 2 0 = 3
-δ 2 1 = 2
-δ 3 0 = 4
-δ 3 1 = 0
-δ 4 _ = 4
-δ _ _ = 5
+d :: State -> Input -> State
+d 0 0 = 1
+d 0 1 = 2
+d 1 0 = 4
+d 1 1 = 2
+d 2 0 = 3
+d 2 1 = 2
+d 3 0 = 4
+d 3 1 = 0
+d 4 _ = 4
+d _ _ = 5
 
 --usw.
 tab :: Table
 tab = [((i,j),False) | i <- q, j <- (delete i q)]
 
-m = map (mark el) tab
+m =  filter  (\x -> snd x == False) $ doo $ map (mark el) tab
 
-el (a,b) 
- | a `elem` f && (not $ b `elem` f) = True
- | b `elem` f && (not $ a `elem` f) = True
+el (a,b)
+ | a `elem` f && (not $ b `elem` f) || b `elem` f && (not $ a `elem` f) = True
  | otherwise = False
 
-mark f (a,b) 
+mark f (a,b)
  | f a = (a,True)
- | otherwise = (a,False)
+ | otherwise = (a,b)
 
-doo x 
+doo x
  | new == x = x
  | otherwise = doo new
  where
   new = map (mark (test x)) x
-   where
-    g d@((a,b),c)  
-     | c == True = d
-     | test x (a,b) == True = ((a,b),True)
-     | otherwise = d
-
 
 test :: Table -> (State,State) -> Bool
+test x y@(a,b) = foldl (||) False (map(\p -> fromMaybe False (lookup p x)) (map (\o -> ((d (fst (fst o)) (snd o)),(d (snd (fst o)) (snd o)))) (zip [y] sigma)))
 
-test x y@(a,b) =  foldl (||) False (map(\p -> fromMaybe False (lookup p x)) (map (\o -> ((δ (fst (fst o)) (snd o)),(δ (snd (fst o)) (snd o)))) (zip [y] sigma)))
