d2cca352f3d04fef19cb5ece1daeb774c02dbd48
Paul Walger <metaxy@walger.name>
2013-12-03 22:36:36 +0100

    somehow working

--- shed.hs
+++ shed.hs
@@ -3,7 +3,8 @@ import Data.Ord
 data Process = Process {
     name :: Char,
     start :: Int,
-    run :: Int 
+    run :: Int,
+    lev :: Int
 } deriving (Show)
 
 instance Eq Process where
@@ -22,11 +23,11 @@ emptyState = State 0 (timeDown $ head example) [] [[]] 0
 
 example :: [Process]
 example = [
-    (Process 'A' 0 8) 
-    ,(Process 'B' 1 2)
-    ,(Process 'C' 3 7)
-    ,(Process 'D' 7 13)
-    ,(Process 'E' 9 2)]
+    (Process 'A' 0 8 0) 
+    ,(Process 'B' 1 2 0)
+    ,(Process 'C' 3 7 0)
+    ,(Process 'D' 7 13 0)
+    ,(Process 'E' 9 2 0)]
 
 maxTime = sum $ (map run example)
 
@@ -60,27 +61,46 @@ rr slice s = stateTimeStep $ appendProcess (nextProzess $ time s) $ rr' slice s
 -- sortest job means shortes run time left
 sjn s = stateTimeStep $ sortQueue(comparing run) $ appendProcess (nextProzess $ time s) $ runAsMuchYouWant $ s
 
-mlf s = stateTimeStep $ addToFirstQueue (nextProzess $ time s) $ mlf' 0 s
+--multilevel feedback
+mlf max s = stateTimeStep $ addToNQueue (nextProzess $ time s) 0 $ mlf' max 0 s
 
-mlf' level s 
-	| (counter s) == (timeStep level) = nextMlf level s
-	| otherwise = runAsMuchYouWant s
+mlf' max level s 
+	| (counter s) == (timeStep level) = resetCounter $ nextMlf max level s
+	| (run $ active s) <= 0 = resetCounter $ nextMlf max level s
+    | otherwise =  incCounter $ s
 
-nextMlf level s
-	| null $ queue = --fifo
- 	| otherwise = --add active to next queue if not empy
-			removeHeadFromQueues level $ s{active = (head queue)} $ 
-		where
-			queue = drop level $ queues s
+nextMlf max level s
+    | max == level = incCounter $ s
+    | (length $ queues s) <= level = nextMlf max level $ s{queues = (queues s) ++ [[]]}
+	| null queue = nextMlf max (level + 1) s
+ 	| otherwise = n $ addToRightQueue (act s) s
+	        where
+                queue = head $ drop level $ queues s
+                n a = removeHeadFromQueues level $ a{active = setLevel level $ (head queue)}  
 
-addToFirstQueue p s = s{queues = (addToFirstQueue' p $ queues s)}
 
-addToFirstQueue' ::  [Process] -> [[Process]] ->  [[Process]]
-addToFirstQueue' p x =  ((head x) ++ p):(tail x)
+addToRightQueue [] s = s
+addToRightQueue [x] s = addToNQueue [x] ((lev x) + 1) s
 
-removeHeadFromQueues level s = s
+-- add a process to the nth queue
+addToNQueue p level s = s{queues = (addToNQueue' p level $ queues s)}
+addToNQueue' p = modN ((++) p)
 
-timeStep i = 2 ** i
+--remove head process to the nth queue
+removeHeadFromQueues level s = s{queues = modN (drop 1) level $ queues s}
+
+modN f level q 
+    | level == length q = q ++ [f []]
+    | level < (length q) = start ++ [middle] ++ end
+    | otherwise = error "to muich"
+    where
+        start = take (level) q
+        middle' = q !! (level)
+        end = drop (level+1) q
+        middle = f middle'
+
+timeStep :: Int -> Int
+timeStep i = 2 ^ i
 
 
 -- default time counter
@@ -96,14 +116,16 @@ appendProcess p s = s{queue = (queue s) ++ p}
 --add a process to begin of queue
 prependProcess p s = s{queue = p ++ (queue s)}
 
-incCounter s = s{counter=(counter s) + 1}
+incCounter s = s{counter = (counter s) + 1}
 
-resetCounter s = s{counter=0}
+resetCounter s = s{counter = 0 }
 
 sortQueue f s = s{queue = (sortBy f (queue s))}
 -- time counter for a process
 timeDown :: Process -> Process
-timeDown (Process a b time) = Process a b (time-1)
+timeDown p = p{run = (run p) - 1}
+
+setLevel level p = p{lev = level}
 
 -- which process to activate
 nextProzess time = filter (\x -> start x == (time+1)) example
@@ -123,8 +145,9 @@ simulate :: (State -> State) -> [State]
 simulate i = take maxTime $ iterate i emptyState
 
 
-waitTime states = map (\x -> length $ filter (\y -> elem x (queue y)) states) example
+waitTime states = map (\x -> length $ filter (\y -> (elem x (queue y)) ||  (any (elem x) (queues y))) states) example
 respTime states = map (\(x,y) -> x+y) $ zip (map run example) (waitTime states)
+
 
 mean x = (fromIntegral $ sum x) / (fromIntegral $ length x)
 
@@ -145,8 +168,11 @@ showState s = do
     putStrLn "---------"
     putStr "running: "
     print $ active s
-    putStr "queue:"
+    putStr "queue: "
     mapM_ putStr (map (show . name) (queue s))
+    putStrLn ""
+    putStr "queues: "
+    mapM_ putStr (map (map (name)) (queues s))
     putStrLn ""
     return ()
 
